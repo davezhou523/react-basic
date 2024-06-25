60ec1f59927adaeba7ba23959413c16115501839
davezhou523 <davezhou523@gmail.com>
2024-06-25 22:49:40 +0800

    init

--- src/store/index.js
+++ src/store/index.js
@@ -2,11 +2,13 @@ import {configureStore} from "@reduxjs/toolkit";
 //导入子模块reducer
 import counterStore from "./modules/counterStore";
 import channelStore from "./modules/channelStore";
+import foodsReducer from "./modules/takeaway";
 //创建根store 组合子模块
  const store=configureStore({
     reducer:{
         mycounter:counterStore,
-        mychannel:channelStore
+        mychannel:channelStore,
+        myfoods:foodsReducer
     }
 })
 export default store;
